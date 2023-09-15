467780b376abb6a5a1e9b64cf792cfbe38508574
sangtrandev00 <nhatsang0101@gmail.com>
2023-09-15 16:52:56 +0700

    Add some code, dependencies for projects,UI lib,...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    loadChildren: () =>
+      import('./modules/admin/categories/categories.module').then(
+        (m) => m.CategoriesModule
+      ),
+  },
+  {
+    path: 'admin/products',
+    loadChildren: () =>
+      import('./modules/admin/products/products.module').then(
+        (m) => m.ProductsModule
+      ),
+  },
+  {
+    path: 'admin/users',
+    loadChildren: () =>
+      import('./modules/admin/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'admin/orders',
+    loadChildren: () =>
+      import('./modules/admin/orders/orders.module').then(
+        (m) => m.OrdersModule
+      ),
+  },
+  {
+    path: 'admin/dashboard',
+    loadChildren: () =>
+      import('./modules/admin/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SideBarComponent } from './shared/components/layout/side-bar/side-bar.component';
+import { HeaderComponent } from './shared/components/layout/header/header.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SideBarComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
