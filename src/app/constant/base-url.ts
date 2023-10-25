4153127c01edb35ea8d527fa466050f45fdc120a
sangtrandev00 <nhatsang0101@gmail.com>
2023-10-25 08:38:09 +0700

    Update Angular base URL for backend api,..

--- src/app/constant/base-url.ts
+++ src/app/constant/base-url.ts
@@ -1,3 +1,3 @@
-// export const BACKEND_DOMAIN = 'http://angular-api.trannhatsang.com';
+export const BACKEND_DOMAIN = 'https://angular-rest-api.trannhatsang.com/';
 // export const BACKEND_DOMAIN = 'http://localhost:8080';
-export const BACKEND_DOMAIN = 'https://angular-app-service-api.onrender.com';
+// export const BACKEND_DOMAIN = 'https://angular-app-service-api.onrender.com';
